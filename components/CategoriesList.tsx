ca2800412b53fd6d3ccad2bfce019e23fe0283fe
Matthew Dsouza <matthewdsouza.one@gmail.com>
2023-10-11 00:46:14 +0530

    first commit

---
+++ components/CategoriesList.tsx
@@ -0,0 +1,13 @@
+import { categoriesData } from "@/data";
+import Link from "next/link";
+
+export default function CategoriesList() {
+  return (
+    <div className="flex gap-2 text-sm flex-wrap">
+      {categoriesData &&
+        categoriesData.map((category) => (
+          <Link className="px-4 py-1 rounded-md bg-slate-800 text-white cursor-pointer" href={`/category/${category.name}`}>{category.name}</Link>
+        ))}
+    </div>
+  );
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className="py-3 text-center">This website is build and designed by Matthew Dsouza © 2023</footer>;
+}
