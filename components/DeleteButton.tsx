03e0cd9c8a42b51708bd9523b6c831c8f17b9da8
Matthew Dsouza <matthewdsouza.one@gmail.com>
2023-10-28 13:37:42 +0530

    added DELETE function for dashboard page

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,3 +1,32 @@
-export default function DeleteButton () {
-    return <button className="text-red-600">Delete</button>;
+"use client";
+
+export default function DeleteButton({ id }: { id: string }) {
+  const handleDelete = async () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this post?"
+    );
+
+    if (confirmed) {
+      try {
+        const res = await fetch(`/api/posts/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-type": "application/json",
+          },
+        });
+
+        if (res.ok) {
+          console.log("Post deleted.");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <button onClick={handleDelete} className="text-red-600">
+      Delete
+    </button>
+  );
 }
